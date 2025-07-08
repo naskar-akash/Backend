8eaeb9bc8ad519ea1e59dd9690e4c2276a9641b7
naskar-akash <naskar.akash1947@gmail.com>
2025-07-08 15:55:35 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require("express");
+import express from 'express'
+import cors from 'cors'
 const app = express();
 const port = 3000;
+
+//middlewares
+app.use(cors())
+app.use(express())
 
 app
   .get("/", (req, res) => {
